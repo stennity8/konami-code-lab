69e33b16faba281a33355acb788d0a3b8ea9b4f6
stennity8 <42423580+stennity8@users.noreply.github.com>
2019-03-05 19:10:42 -0500

    Added solutions

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,38 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let idx = 0
+
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+
+    idx = (codes[idx] === key) ? ++idx : 0
+
+
+    if (idx === codes.length) {
+      window.alert("Hurray!");
+      idx = 0
+    }
+
+  });
+}
+
+//My Solution
+function init() {
+  let index = 0;
+  const input = document.querySelector('body');
+
+  input.addEventListener('keydown', function (e) {
+    const key = e.key;
+    if (key === codes[index]) {
+      index++;
+
+      if (index === codes.length) {
+        alert("Hurray! You have correctly entered the Konami!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
